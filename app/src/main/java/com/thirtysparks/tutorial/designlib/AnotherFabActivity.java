9e915f2bb6849eaa7c80eb64823c62d7426c69f7
Ryan Leung <rwcleung@gmail.com>
2015-09-04 11:39:13 +0800

    Add `AnotherFabActivity` with `FloatingActionButtonBehavior` using the open source `FloatingActionButton` implementation.

---
+++ app/src/main/java/com/thirtysparks/tutorial/designlib/AnotherFabActivity.java
@@ -0,0 +1,51 @@
+package com.thirtysparks.tutorial.designlib;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.clans.fab.FloatingActionButton;
+
+public class AnotherFabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_fab);
+
+        final TextView contentView = (TextView)findViewById(R.id.content_view);
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(contentView, "Snackbar!", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_another_fab, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
