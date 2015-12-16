84e257075760dcad97b364bff8d6fe1f6c579115
Ryan Leung <rwcleung@gmail.com>
2015-12-16 17:20:35 +0800

    Add "FAB" to `AppBarActivity`

--- app/src/main/java/com/thirtysparks/tutorial/designlib/AppBarActivity.java
+++ app/src/main/java/com/thirtysparks/tutorial/designlib/AppBarActivity.java
@@ -1,6 +1,9 @@
 package com.thirtysparks.tutorial.designlib;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.List;
 
@@ -15,6 +19,7 @@ import java.util.List;
 public class AppBarActivity extends AppCompatActivity {
     private List<Contact> mContacts;
     private ContactsAdapter adapter;
+    private RecyclerView rvContacts;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,13 +34,15 @@ public class AppBarActivity extends AppCompatActivity {
         //layoutManager = new GridLayoutManager(this, 3);
         layoutManager = new LinearLayoutManager(this);
 
-        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.recyclerView);
+        rvContacts = (RecyclerView) findViewById(R.id.recyclerView);
         adapter = new ContactsAdapter(mContacts);
         rvContacts.setAdapter(adapter);
         rvContacts.setLayoutManager(layoutManager);
 
         RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
         rvContacts.addItemDecoration(itemDecoration);
+
+        initFab();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,6 +65,26 @@ public class AppBarActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initFab(){
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(rvContacts, "I am snackbar", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                new AlertDialog.Builder(AppBarActivity.this)
+                                        .setMessage("Undo pressed")
+                                        .setNeutralButton("OK", null)
+                                        .show();
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 
     private void addContacts(){
