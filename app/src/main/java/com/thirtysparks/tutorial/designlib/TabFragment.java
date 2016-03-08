69d39fc32ce8b714e2ad90e6b451401c1e5a5c70
Ryan Leung <rwcleung@gmail.com>
2016-03-08 11:30:21 +0800

    Add `TabFragment` and its layout

---
+++ app/src/main/java/com/thirtysparks/tutorial/designlib/TabFragment.java
@@ -0,0 +1,54 @@
+package com.thirtysparks.tutorial.designlib;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TabFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public static TabFragment newInstance(String param1, String param2) {
+        TabFragment fragment = new TabFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public TabFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView param1TextView = (TextView) view.findViewById(R.id.tv_param1);
+        TextView param2TextView = (TextView) view.findViewById(R.id.tv_param2);
+
+        param1TextView.setText(mParam1);
+        param2TextView.setText(mParam2);
+    }
+}
