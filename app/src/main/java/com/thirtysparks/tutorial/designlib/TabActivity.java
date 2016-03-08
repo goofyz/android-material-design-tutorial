ec91aeac90a02c5fd04d1a9ca602c5f28dcda859
Ryan Leung <rwcleung@gmail.com>
2016-03-08 11:33:54 +0800

    setup the `tabLayout` with the `TabPagerAdapter`

--- app/src/main/java/com/thirtysparks/tutorial/designlib/TabActivity.java
+++ app/src/main/java/com/thirtysparks/tutorial/designlib/TabActivity.java
@@ -1,9 +1,10 @@
 package com.thirtysparks.tutorial.designlib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class TabActivity extends AppCompatActivity {
 
@@ -11,5 +12,14 @@ public class TabActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TabPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/com/thirtysparks/tutorial/designlib/TabFragment.java
+++ app/src/main/java/com/thirtysparks/tutorial/designlib/TabFragment.java
@@ -2,7 +2,7 @@ package com.thirtysparks.tutorial.designlib;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
